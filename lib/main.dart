c872c23d4918d57cee74df305e38a323a5064761
Thomas David <thomas.david@student.odisee.be>
2022-07-01 11:38:19 +0200

    decoration with borderRadius

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,18 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
           child: Container(
-            color: Colors.white,
+            // color: Colors.white,
             child: Text('hello'),
+            decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.lightBlue,
+                    width: 5,
+                    style: BorderStyle.solid),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                )),
             height: 100.0,
-            // margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
-            // margin: EdgeInsets.fromLTRB(60.0, 25.0, 35.0, 50.0),
-            margin: EdgeInsets.only(left: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
             width: 100.0,
           ),
         ),
