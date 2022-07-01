184de0a145ca88e0b7bf5ca982a3f74c6bd5a13f
Thomas David <thomas.david@student.odisee.be>
2022-07-01 11:44:47 +0200

    Column with 3 Containers. mainAxisAlignment, verticalDirection

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,61 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
-          child: Container(
-            // color: Colors.white,
-            child: Text('hello'),
-            decoration: BoxDecoration(
-                border: Border.all(
-                    color: Colors.lightBlue,
-                    width: 5,
-                    style: BorderStyle.solid),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                )),
-            height: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
-            width: 100.0,
+          child: Column(
+            // shrink to size of all the containers
+            // mainAxisSize: MainAxisSize.min,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            verticalDirection: VerticalDirection.up,
+            children: [
+              Container(
+                // color: Colors.white,
+                child: Text('hello'),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.lightBlue,
+                        width: 5,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    )),
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
+                width: 100.0,
+              ),
+              Container(
+                // color: Colors.white,
+                child: Text('hello'),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.lightBlue,
+                        width: 5,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    )),
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
+                width: 100.0,
+              ),
+              Container(
+                // color: Colors.white,
+                child: Text('hello'),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.lightBlue,
+                        width: 5,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    )),
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 100.0),
+                width: 100.0,
+              ),
+            ],
           ),
         ),
       ),
